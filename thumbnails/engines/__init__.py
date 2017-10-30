cdf88361adaec42d9b9b3c3f1b2c9f667bf9c5e7
Benjamin Schollnick <Benjamin@schollnick.net>
2017-10-29 21:51:15 -0400

    Add base import since docs test was failing in TOX

--- thumbnails/engines/__init__.py
+++ thumbnails/engines/__init__.py
@@ -3,3 +3,4 @@ from thumbnails.engines.dummy import DummyEngine  # noqa
 from thumbnails.engines.pillow_engine import PillowEngine  # noqa
 from thumbnails.engines.wand_engine import WandEngine  # noqa
 from thumbnails.engines.pgmagick_engine import PgmagickEngine  # noqa
+from thumbnails.engines.base import *
